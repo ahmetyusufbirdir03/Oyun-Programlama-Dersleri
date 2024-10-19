c7eb0e8c8e20362c10db355009366a116de475f1
ahmetyusufbirdir03 <168556979+ahmetyusufbirdir03@users.noreply.github.com>
2024-10-19 14:14:44 +0300

    Ekleme

---
+++ C# Kodları/Hafta-3/Player_sc.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_sc : MonoBehaviour
+{
+    public int playerHealth = 3;
+    public float speed = 5;
+    public float horizontal;
+    public float vertical;
+    float fireRate = 0.5f;
+    float nextFire = 0f;
+    public GameObject laserPrefab;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+        if(Input.GetKeyDown(KeyCode.Space) && Time.time > nextFire)
+        {
+            Instantiate(laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }        
+    }
+
+    void CalculateMovement()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        
+        Vector3 direction = new Vector3(horizontal, vertical, 0);
+        transform.Translate(direction * Time.deltaTime * speed);
+    }
+
+    public void Damage(){
+        this.playerHealth--;
+        if(playerHealth == 0){
+            Destroy(this.gameObject);
+        }
+    }
+}
